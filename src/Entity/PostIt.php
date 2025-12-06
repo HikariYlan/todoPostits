622f1c338081b63d1e52a645b4adda2c1533647a
HikariYlan <ylan.nicolas52600@gmail.com>
2025-12-06 12:46:05 +0100

    feat: PostIt entity

---
+++ src/Entity/PostIt.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Entity;
+
+use App\Enum\Status;
+use App\Repository\PostItRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PostItRepository::class)]
+class PostIt
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 150)]
+    private string $title;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $description = null;
+
+    #[ORM\Column]
+    private \DateTime $creationDate;
+
+    #[ORM\Column(nullable: true)]
+    private ?\DateTime $finishDate = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?\DateTime $dueDate = null;
+
+    #[ORM\Column(type: 'string', enumType: Status::class)]
+    private string $status;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getCreationDate(): \DateTime
+    {
+        return $this->creationDate;
+    }
+
+    public function setCreationDate(\DateTime $creationDate): static
+    {
+        $this->creationDate = $creationDate;
+
+        return $this;
+    }
+
+    public function getFinishDate(): ?\DateTime
+    {
+        return $this->finishDate;
+    }
+
+    public function setFinishDate(?\DateTime $finishDate): static
+    {
+        $this->finishDate = $finishDate;
+
+        return $this;
+    }
+
+    public function getDueDate(): ?\DateTime
+    {
+        return $this->dueDate;
+    }
+
+    public function setDueDate(?\DateTime $dueDate): static
+    {
+        $this->dueDate = $dueDate;
+
+        return $this;
+    }
+
+    public function getStatus(): string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(string $status): static
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+}
