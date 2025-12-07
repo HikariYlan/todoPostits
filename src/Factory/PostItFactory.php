a236d9bd4aa742c23edd06ab4b3886c434402316
HikariYlan <ylan.nicolas52600@gmail.com>
2025-12-07 01:49:06 +0100

    feat: add PostItFactory for creating PostIt entities with default values

---
+++ src/Factory/PostItFactory.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Factory;
+
+use App\Entity\PostIt;
+use App\Enum\Status;
+use App\Story\UserStory;
+use Zenstruck\Foundry\LazyValue;
+use Zenstruck\Foundry\Persistence\PersistentProxyObjectFactory;
+
+/**
+ * @extends PersistentProxyObjectFactory<PostIt>
+ */
+final class PostItFactory extends PersistentProxyObjectFactory
+{
+    public function __construct()
+    {
+    }
+
+    public static function class(): string
+    {
+        return PostIt::class;
+    }
+
+    protected function defaults(): array|callable
+    {
+        return [
+            'owner' => LazyValue::new(fn () => UserFactory::createOne()),
+            'status' => self::faker()->randomElement(Status::cases()),
+            'title' => self::faker()->text(150),
+        ];
+    }
+
+    protected function initialize(): static
+    {
+        return $this->afterInstantiate(function(PostIt $postIt): void {
+            if ($postIt->getStatus() == Status::FINISHED) $postIt->setFinishDate(new \DateTime("+2 days"));
+        })
+        ;
+    }
+}
