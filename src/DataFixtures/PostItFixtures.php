d597a3dd0473b3395cd1a68ee01c2e5912d82a3d
HikariYlan <ylan.nicolas52600@gmail.com>
2025-12-07 01:49:13 +0100

    feat: add PostItFixtures for loading sample PostIt data with dependencies

---
+++ src/DataFixtures/PostItFixtures.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Factory\PostItFactory;
+use App\Factory\UserFactory;
+use App\Story\UserStory;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+
+class PostItFixtures extends Fixture implements DependentFixtureInterface
+{
+    public function load(ObjectManager $manager): void
+    {
+        PostItFactory::createMany(50, function () {
+            if (random_int(0, 1) == 1) $dueDate = new \DateTime('+20 days');
+            return [
+                "owner" => UserStory::getRandom('random_users'),
+                "dueDate" => $dueDate ?? null,
+            ];
+        });
+
+        PostItFactory::createMany(10, function () {
+            if (random_int(0, 1) == 1) $dueDate = new \DateTime('+20 days');
+            return [
+                "owner" => UserFactory::findBy([ 'username' => 'ylan'])[0],
+                "dueDate" => $dueDate ?? null,
+            ];
+        });
+    }
+
+    public function getDependencies(): array
+    {
+        return [
+            UserFixtures::class,
+        ];
+    }
+}
