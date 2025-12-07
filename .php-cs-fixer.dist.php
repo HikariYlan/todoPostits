a4ec0de1d5ffe4f76c053b8c54831ab307856ea8
HikariYlan <ylan.nicolas52600@gmail.com>
2025-12-07 01:54:56 +0100

    feat: install PHP CS Fixer

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,15 @@
+<?php
+
+$finder = (new PhpCsFixer\Finder())
+    ->in(__DIR__)
+    ->exclude(['var'])
+;
+
+return (new PhpCsFixer\Config())
+    ->setRules([
+        '@Symfony' => true,
+    ])
+    ->setFinder($finder)
+    ->setParallelConfig(new PhpCsFixer\Runner\Parallel\ParallelConfig(4))
+    ->setCacheFile('var/.php-cs-fixer.cache')
+;
