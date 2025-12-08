5e0b078533bf3a7bae6680b94f3010ec8c75a773
HikariYlan <ylan.nicolas52600@gmail.com>
2025-12-08 18:27:52 +0100

    feat: add PostIt form type for creating and editing PostIt notes

---
+++ src/Form/PostItType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\PostIt;
+use App\Entity\User;
+use App\Enum\Status;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\EnumType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PostItType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            ->add('description')
+            ->add('dueDate', DateTimeType::class, ['required' => false])
+            ->add('status', EnumType::class, ['class' => Status::class])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => PostIt::class,
+        ]);
+    }
+}
