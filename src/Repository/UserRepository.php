af00489a6c26a3bac0d671600ca4434dcad62661
HikariYlan <ylan.nicolas52600@gmail.com>
2025-12-08 01:46:38 +0100

    feat: add method to retrieve user SteamID by username

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -43,28 +43,13 @@ class UserRepository extends ServiceEntityRepository implements PasswordUpgrader
         $this->getEntityManager()->flush();
     }
 
-    //    /**
-    //     * @return User[] Returns an array of User objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('u')
-    //            ->andWhere('u.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('u.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
-
-    //    public function findOneBySomeField($value): ?User
-    //    {
-    //        return $this->createQueryBuilder('u')
-    //            ->andWhere('u.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
+    public function getUserSteamIDFromUsername(string $username)
+    {
+        return $this->createQueryBuilder('u')
+            ->select('u.steamID')
+            ->where('u.username = :username')
+            ->setParameter('username', $username)
+            ->getQuery()
+            ->getResult()[0]['steamID'];
+    }
 }
