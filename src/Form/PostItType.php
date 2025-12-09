23c765fc7845ab54b7873813b2870260e0baee97
HikariYlan <ylan.nicolas52600@gmail.com>
2025-12-09 19:01:10 +0100

    feat: enhance PostIt form with improved labels and status choices

--- src/Form/PostItType.php
+++ src/Form/PostItType.php
@@ -3,9 +3,7 @@
 namespace App\Form;
 
 use App\Entity\PostIt;
-use App\Entity\User;
 use App\Enum\Status;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\EnumType;
@@ -17,10 +15,27 @@ class PostItType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('description')
-            ->add('dueDate', DateTimeType::class, ['required' => false])
-            ->add('status', EnumType::class, ['class' => Status::class])
+            ->add('title', options: [
+                'label' => 'Title',
+            ])
+            ->add('description', options: [
+                'label' => 'Description',
+            ])
+            ->add('dueDate', DateTimeType::class, [
+                'required' => false,
+                'label' => 'Due Date',
+            ])
+            ->add('status', EnumType::class, [
+                'class' => Status::class,
+                'label' => 'Status',
+                'choice_label' => fn (Status $status) => str_replace('_', ' ', $status->value),
+                'choices' => [
+                    'PENDING' => Status::PENDING,
+                    'TO DO' => Status::TO_DO,
+                    'ON GOING' => Status::ON_GOING,
+                    'FINISHED' => Status::FINISHED,
+                ],
+            ])
         ;
     }
 
