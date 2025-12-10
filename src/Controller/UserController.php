c48b4896ec55193fce422fd028670a88832455b4
HikariYlan <ylan.nicolas52600@gmail.com>
2025-12-10 15:47:49 +0100

    feat: create UserController for managing user settings

---
+++ src/Controller/UserController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\UserSettingsType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class UserController extends AbstractController
+{
+    #[Route('/settings', name: 'app_user')]
+    public function index(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $user = $this->getUser();
+        $form = $this->createForm(UserSettingsType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_sticky_board');
+        }
+
+        return $this->render('user/index.html.twig', [
+            'form' => $form,
+            'submit_label' => 'Update your profile',
+        ]);
+    }
+}
