225216b438ecc855689841f132d8d4c674139a2d
HikariYlan <ylan.nicolas52600@gmail.com>
2025-12-19 19:57:26 +0100

    feat: avatar display route

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -41,4 +41,28 @@ final class UserController extends AbstractController
             'submit_label' => 'Update your profile',
         ]);
     }
+
+    #[Route('/user/{id}/avatar', name: 'app_user_avatar')]
+    public function avatar(User $user): Response
+    {
+        if (!$user->getAvatar()) {
+            throw $this->createNotFoundException('No avatar found for this user.');
+        }
+
+        $avatarData = $user->getAvatar();
+
+        if (is_resource($avatarData)) {
+            $avatarData = stream_get_contents($avatarData);
+        }
+
+        $file_info = finfo_open(FILEINFO_MIME_TYPE);
+        $mimeType = finfo_buffer($file_info, $avatarData);
+        finfo_close($file_info);
+
+        $response = new Response($avatarData);
+        $response->headers->set('Content-Type', $mimeType ?: 'image/jpeg');
+        $response->headers->set('Cache-Control', 'public, max-age=86400');
+
+        return $response;
+    }
 }
