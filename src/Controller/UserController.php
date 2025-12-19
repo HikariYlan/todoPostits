f18b83b54bc8f84286ffd62f59540a42bf147d67
HikariYlan <ylan.nicolas52600@gmail.com>
2025-12-19 19:20:30 +0100

    feat: modify form handling for user's avatar

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use App\Form\UserSettingsType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -14,11 +16,20 @@ final class UserController extends AbstractController
     #[Route('/settings', name: 'app_user_settings')]
     public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
+        /** @var User $user */
         $user = $this->getUser();
         $form = $this->createForm(UserSettingsType::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            /** @var UploadedFile|null $avatarFile */
+            $avatarFile = $form->get('avatar')->getData();
+
+            if ($avatarFile) {
+                $avatarContent = file_get_contents($avatarFile->getPathname());
+                $user->setAvatar($avatarContent);
+            }
+
             $entityManager->persist($user);
             $entityManager->flush();
 
