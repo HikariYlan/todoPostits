676287f7d92b46457f7295b7b8997d93157d2149
HikariYlan <ylan.nicolas52600@gmail.com>
2025-12-21 00:46:58 +0100

    feat: add color form field

--- src/Form/TagFormType.php
+++ src/Form/TagFormType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Tag;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -11,8 +12,62 @@ class TagFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        $colors = [
+            'red' => '#ef4444',
+            'orange' => '#f97316',
+            'amber' => '#f59e0b',
+            'yellow' => '#eab308',
+            'lime' => '#84cc16',
+            'green' => '#22c55e',
+            'emerald' => '#10b981',
+            'teal' => '#14b8a6',
+            'cyan' => '#06b6d4',
+            'sky' => '#0ea5e9',
+            'blue' => '#3b82f6',
+            'indigo' => '#6366f1',
+            'violet' => '#8b5cf6',
+            'purple' => '#a855f7',
+            'fuchsia' => '#d946ef',
+            'pink' => '#ec4899',
+            'rose' => '#f43f5e',
+            'slate' => '#64748b',
+            'gray' => '#6b7280',
+            'zinc' => '#71717a',
+            'neutral' => '#737373',
+            'stone' => '#78716c',
+        ];
+
         $builder
             ->add('name')
+            ->add('color', ChoiceType::class, [
+                'choices' => [
+                    'red' => 'red',
+                    'orange' => 'orange',
+                    'amber' => 'amber',
+                    'yellow' => 'yellow',
+                    'lime' => 'lime',
+                    'green' => 'green',
+                    'emerald' => 'emerald',
+                    'teal' => 'teal',
+                    'cyan' => 'cyan',
+                    'sky' => 'sky',
+                    'blue' => 'blue',
+                    'indigo' => 'indigo',
+                    'violet' => 'violet',
+                    'purple' => 'purple',
+                    'fuchsia' => 'fuchsia',
+                    'pink' => 'pink',
+                    'rose' => 'rose',
+                    'slate' => 'slate',
+                    'gray' => 'gray',
+                    'zinc' => 'zinc',
+                    'neutral' => 'neutral',
+                    'stone' => 'stone',
+                ],
+                'choice_attr' => function ($choice, $key, $value) use ($colors) {
+                    return ['style' => 'color: '.$colors[$value].'; font-weight: bold;'];
+                },
+            ])
         ;
     }
 
