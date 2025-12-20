f7645ec50c56653581b43dad5298735915457605
HikariYlan <ylan.nicolas52600@gmail.com>
2025-12-20 22:09:40 +0100

    feat: display owned tags

--- src/Controller/TagController.php
+++ src/Controller/TagController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Tag;
+use App\Entity\User;
 use App\Form\TagFormType;
+use App\Repository\TagRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -12,10 +14,22 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class TagController extends AbstractController
 {
+    #[Route('/tags', name: 'app_tag')]
+    public function index(TagRepository $repository): Response
+    {
+        /** @var User $user */
+        $user = $this->getUser();
+
+        return $this->render('tag/index.html.twig', [
+            'tags' => $repository->getOwnedTags($user->getId()),
+        ]);
+    }
+
     #[Route('/tag/new', name: 'app_tag_create')]
-    public function index(Request $request, EntityManagerInterface $manager): Response
+    public function createTag(Request $request, EntityManagerInterface $manager): Response
     {
         $tag = new Tag();
+        $tag->setOwner($this->getUser());
 
         $form = $this->createForm(TagFormType::class, $tag);
         $form->handleRequest($request);
